3bb9db198d946220bfad5f26713c99f730ceedc1
Tom Yeh <doubleshow@gmail.com>
2015-02-15 22:16:29 -0700

    build, update baseurl

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # yelp-jquery
 
 Yelp Site Re-built using JQuery
+
+
+# Deploy
+
+    $ wintersmith build
+
+    $ git commit -a -m 'build'
+
+    $ git subtree push --prefix build origin gh-pages
--- contents/js/doctor/view.js
+++ contents/js/doctor/view.js
@@ -2,12 +2,12 @@ function viewDoctor(business_id){
 
     console.log('viewing doctor id = ', business_id)
 
-    $.get("/yelp-ws/data/doctors.json.data", function(data) {
+    $.get("/yelp-jquery/data/doctors.json.data", function(data) {
 
         var doctors = JSON.parse(data)
         var doctor = _.find(doctors, {business_id: business_id})
 
-        $.get("/yelp-ws/templates/viewDoctor.jade", function(template) {
+        $.get("/yelp-jquery/templates/viewDoctor.jade", function(template) {
 
             var html = jade.render(template, {item: doctor})
             

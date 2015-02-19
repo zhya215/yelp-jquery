e70803ca2ac398c19903f15113a69bef099e7c78
mazdeh <vahid.mazdeh@gmail.com>
2015-02-18 17:50:26 -0700

    listUser.js, viewUser.js, listUser.jade, viewUser.jade are all done.

--- contents/js/user/list.js
+++ contents/js/user/list.js
@@ -1,4 +1,23 @@
 function listUsers(){
 
-    alert('todo')
+    console.log('listing doctors')
+
+    $.get("/yelp-jquery/data/users.json.data", function(data) {
+
+        var users = JSON.parse(data)
+
+        $.get("/yelp-jquery/templates/listUsers.jade", function(template) {
+
+            // render the template
+            var html = jade.render(template, {items: users})            
+
+            // assign the rendered html to the dom element whose id is #list
+            $("#list").html(html)
+
+            // load the first doctor to view
+            viewUser(users[0].user_id)
+
+        })
+
+    })
 }

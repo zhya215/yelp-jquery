298a78ae0a1656bc164719ccada6292fd172715d
Peyman Mortazavi <pmortazavi@cailmobility.com>
2015-02-20 12:42:44 -0700

    build

--- build/js/restaurant/list.js
+++ build/js/restaurant/list.js
@@ -1,4 +1,24 @@
 function listRestaurants(){
 
-    alert('todo')
+  console.log('listing restaurants')
+
+    $.get("/yelp-jquery/data/restaurants.json.data", function(data) {
+
+        var restaurants = JSON.parse(data)
+
+        $.get("/yelp-jquery/templates/listRestaurants.jade", function(template) {
+
+            // render the template
+            var html = jade.render(template, {items: restaurants})            
+
+            // assign the rendered html to the dom element whose id is #list
+            $("#list").html(html)
+
+            // load the first restaurant to view
+            viewRestaurant(restaurants[0].business_id)
+
+        })
+
+    })
+
 }
--- build/js/user/view.js
+++ build/js/user/view.js
@@ -1,4 +1,21 @@
 function viewUser(user_id){
 
-    //alert('todo')
+    console.log('viewing doctor id = ', user_id)
+
+    $.get("/yelp-jquery/data/users.json.data", function(data) {
+
+        var users = JSON.parse(data)
+        var user = _.find(users, {user_id: user_id})
+
+        $.get("/yelp-jquery/templates/viewUser.jade", function(template) {
+
+            var html = jade.render(template, {item: user})
+            
+            $("#details").html(html)
+
+        })
+
+    })
+
 }
+
